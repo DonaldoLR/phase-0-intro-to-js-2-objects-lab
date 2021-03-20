d6746c546986a01a3ac3906e4ed20ed3b268fd00
github username <github email address>
2021-03-20 12:15:00 -0400

    Learned about objects

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const employee = {
+    name: "Donaldo",
+    streetAddress: '123 FakeLand'
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    // Copies passed object into a new object
+    const newObject = {...obj};
+    
+    newObject[key] = value;
+
+    return newObject;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+     obj[key] = value;
+
+     return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    const newObj = {...obj};
+
+    delete newObj[key]
+
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key]
+    return obj
+}
